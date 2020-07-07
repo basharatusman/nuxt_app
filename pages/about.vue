f7c2d0dd2bef64e877d236dc25929cb6c6dcb821
basharatusman <basharatusman.a@gmail.com>
2020-07-07 18:17:22 -0400

    Dad Jokes Vue Nuxt app

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About App</h1>
+    <p>This is an app that displays api dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About us",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "best place for dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h2>Welcome to the best corny dad joke app on the web</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  head() {
+    return {
+      title: "Home",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Weclome to the best place for dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
